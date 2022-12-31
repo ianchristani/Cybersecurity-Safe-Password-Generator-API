0c892bb1ad1bb50a65c5bd2ae822791f944066a2
ianchristani <ianchristani@gmail.com>
2022-12-31 12:51:19 -0300

    starting the project.

---
+++ t.py
@@ -0,0 +1,10 @@
+import random
+
+letters=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','x','y','w','z']
+
+
+index=random.randint(0,len(letters))
+print(index)
+psswrd=letters.index(index)
+
+print(psswrd)
